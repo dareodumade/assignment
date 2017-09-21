bf34e255f2ea23d0028b4da2128fb92c8c965e3f
dray <dare.odumade@yahoo.com>
2017-09-21 02:00:06 +0000

    updated output

--- assignmentnew.py
+++ assignmentnew.py
@@ -2,19 +2,21 @@ class Todd:
 	number_of_fellows = 0
 
 	def __init__(self, fellow=[]):
-		if Todd.number_of_fellows < 5:
+		if Todd.number_of_fellows < 4:
 
 			self.fellow=fellow 
 			Todd.number_of_fellows = Todd.number_of_fellows + 1
 		else:
-			print("sorry, you can't take anymore fellows")
+			print("sorry, we can't afford to hire anymore fellows")
+
 
 Todd()
-print(Todd.number_of_fellows)
+print("Pascal, DRC {}".format(Todd.number_of_fellows))
+Todd() 
+print("Andrew, USA{}".format(Todd.number_of_fellows))
+Todd()
+print("Miishe, GH/Murika {}".format(Todd.number_of_fellows))
+Todd()
+print("Simphiwe, Africa Sur {}".format(Todd.number_of_fellows))
 Todd()
 print(Todd.number_of_fellows)
-Todd()
-print(Todd.number_of_fellows)
-Todd()
-print(Todd.number_of_fellows)
-Todd()
